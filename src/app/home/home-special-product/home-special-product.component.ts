64d02609e732d214d071d0f2e7ba7aad7a2224f1
ZahraAbdolalian <abdolalianzahra@gmail.com>
2023-12-11 10:20:15 +0330

    add section special products

--- src/app/home/home-special-product/home-special-product.component.ts
+++ src/app/home/home-special-product/home-special-product.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/post.model';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-home-special-product',
   templateUrl: './home-special-product.component.html',
   styleUrls: ['./home-special-product.component.scss']
 })
-export class HomeSpecialProductComponent {
+export class HomeSpecialProductComponent implements OnInit{
 
+  starsArray: any[] = new Array(5);
+  productsArray : Post[] = []
+
+  constructor (private apiService : ApiService){}
+
+  ngOnInit(): void {
+      this.apiService.getSomeProducts(5)
+      .subscribe(data => {
+        this.productsArray = data
+        console.log(data);
+      })
+  }
 }
