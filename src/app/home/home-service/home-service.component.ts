5f070e8a38c65d14f728bcf30fe7869fcd356b0b
ZahraAbdolalian <abdolalianzahra@gmail.com>
2023-12-11 10:50:19 +0330

    add section service

---
+++ src/app/home/home-service/home-service.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-service',
+  templateUrl: './home-service.component.html',
+  styleUrls: ['./home-service.component.scss']
+})
+export class HomeServiceComponent {
+}
